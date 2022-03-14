a82b3aefca8501224e8440f89e2d14d1c0a016e2
hotpink <8628988+miss-hu@user.noreply.gitee.com>
2022-03-14 13:29:08 +0800

    初始化文件

--- src/main.js
+++ src/main.js
@@ -3,10 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入element-ui组件库
+import ElementUI from 'element-ui'
+
+// 导入elementUI组件库样式
+import "element-ui/lib/theme-chalk/index.css"
+
 Vue.config.productionTip = false
 
+// 使用elementUI组件库
+Vue.use(ElementUI)
+
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
